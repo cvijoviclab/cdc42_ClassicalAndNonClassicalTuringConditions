354223b66892395e8e7c32b17787fdd6851553ee
cvijoviclab <cvijoviclab@gmail.com>
2020-10-13 08:00:29 +0200

    Change how csv files are saved using panda

--- Code/FEMFD_AlgorithmForSolvingTheRDsystem_FEniCSandPython/ScriptsToRun/mainFile.py
+++ Code/FEMFD_AlgorithmForSolvingTheRDsystem_FEniCSandPython/ScriptsToRun/mainFile.py
@@ -39,6 +39,7 @@ print('=========================================================================
 # Nice package to create vectors, innit?
 import numpy as np
 import os
+import pandas as pd 
 #import pymp
 # The proportion of the surface are over the volume
 a = 3
@@ -73,8 +74,8 @@ for dataIter in range(2): #Dont do special cases as for now
         c2 = 0.45
         u0 = 1.2263
         v0 = 0.6276
-        strBase = '../../../Results/increasingGamma/Classical/21'
-        os.mkdir("../../../Results/increasingGamma/Classical/21")
+        strBase = "../../../Results/increasingGamma/Classical/21"        
+        os.mkdir(strBase)
         
     else: # Non-classic, increasing d
         c1 =  0.05
@@ -82,39 +83,41 @@ for dataIter in range(2): #Dont do special cases as for now
         c2 = 0.15
         u0 = 0.9059
         v0 = 0.9332
-        strBase = '../../../Results/increasingGamma/NonClassical/21'
-        os.mkdir("../../../Results/increasingGamma/NonClassical/21")         
+        strBase = "../../../Results/increasingGamma/NonClassical/21"
+        os.mkdir(strBase)         
         
      # Initial concentraiton cytosolic cdc42, V (i.e. cdc42-GDI)
     V0 = (V0_init - ( a * (u0+v0)) )
     #--------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    # number of diffusion coefficients
+    # number of gamma values corresponding to the relative strength of the reaction
+    # versus diffusion
     gammaVec = np.arange(10,163,10)
+    # Save the gamma vector
+    # convert array into dataframe 
+    DF0 = pd.DataFrame(gammaVec) 
+    # save the dataframe as a csv file 
+    DF0.to_csv(strBase + "gammaVec.csv")    
     #-------------------------------------------------------------------------------------------------------------------------------------------------------
     #-------------------------------------------------------------------------------------------------------------------------------------------------------
     #------------------------------------------------------------------------------------------------------------------------------------------------------- 
-    #--------------------------------------------------------------------------------------------------------------------------------------------------------    # Repat for stochasticity
+    #--------------------------------------------------------------------------------------------------------------------------------------------------------
+    # Repeat for stochasticity
     nuOfRepeats = 1
     print('\tWe repeat %d times!\n\n' % (nuOfRepeats))    
-    # Files for saving the tex-files, hey? Now, we save everything and do the plottung afterwards
-    uMaxAvg_file = open("%s/uMax.csv" % (strBase), "w")
-    uMinAvg_file = open("%s/uMin.csv" % (strBase), "w")
-    ratioPoleAvg_file = open("%s/ratioPole.csv" % (strBase), "w")
-    tPoleAvg_file = open("%s/tPole.csv" % (strBase), "w")
-    gamma_file = open("%s/gamma.csv" % (strBase), "w")        
-    # Define the average parameters
+    # Define the matrices in which we will save four parameters:
+    # uMax, uMin, tPole and ratioPole
     u_maxAvg = np.zeros((len(gammaVec),nuOfRepeats),dtype=np.double)
     u_minAvg = np.zeros((len(gammaVec),nuOfRepeats),dtype=np.double)
     t_poleAvg = np.zeros((len(gammaVec),nuOfRepeats),dtype=np.double)
     ratio_poleAvg = np.zeros((len(gammaVec),nuOfRepeats),dtype=np.double)
     #-------------------------------------------------------------------------------------------------------------------------------------------------------     #-------------------------------------------------------------------------------------------------------------------------------------------------------      
+    # We loop over the number of repetition (for stochasticity in the ICs)
     for innerIndex in range(nuOfRepeats):                     
         # We now loop through the various values of c_1 to run the calculations
         for i in range(len(gammaVec)):
             #--------------------------------------------------------------------------------------------------------------------------------------------------------
             #--------------------------------------------------------------------------------------------------------------------------------------------------------
-            gamma = gammaVec[i] # Relative diffusion of active versus the inactive
+            gamma = gammaVec[i] # Extract the reaction strength
             #=================================================================================================
             #=================================================================================================
             #=================================================================================================
@@ -143,24 +146,32 @@ for dataIter in range(2): #Dont do special cases as for now
             ratio_poleAvg[i,innerIndex] = ratio_pole
             #-----------------------------------------------------------------------------------------
             print('\t\tCalculations are finished!')
-            # Close the two tex-files which we write to as well
+
             
-            
-    for i in range(len(gammaVec)):
-        gamma_file.write("%d\n" % (gammaVec[i])) 
-        for j in range(nuOfRepeats):
-            # SAVE TO FILES
-            uMaxAvg_file.write("%0.5f\t" % (u_maxAvg[i,j]))
-            uMinAvg_file.write("%0.5f\t" % (u_minAvg[i,j]))
-            tPoleAvg_file.write("%0.5f\t" % (t_poleAvg[i,j]))
-            ratioPoleAvg_file.write("%0.5f\t" % (ratio_poleAvg[i,j]))
-    uMaxAvg_file.write("\n")
-    uMinAvg_file.write("\n")
-    tPoleAvg_file.write("\n")
-    ratioPoleAvg_file.write("\n")                         
-    # Close the files, hey?    
-    gamma_file.close()
-    uMaxAvg_file.close()
-    uMinAvg_file.close()
-    tPoleAvg_file.close()
-    ratioPoleAvg_file.close()                  
+    # Finally, we save the matrices in csv files which will
+    # be used for post-processing
+    #---------------------------------------------------------                
+    # u_min
+    # convert array into dataframe 
+    DF1 = pd.DataFrame(u_minAvg) 
+    # save the dataframe as a csv file 
+    DF1.to_csv(strBase + "uMin.csv")
+    #---------------------------------------------------------                
+    # u_max
+    # convert array into dataframe 
+    DF2 = pd.DataFrame(u_maxAvg) 
+    # save the dataframe as a csv file 
+    DF2.to_csv(strBase + "uMax.csv")
+    #---------------------------------------------------------                
+    # t_pole
+    # convert array into dataframe 
+    DF3 = pd.DataFrame(t_poleAvg) 
+    # save the dataframe as a csv file 
+    DF3.to_csv(strBase + "tPole.csv")
+    #---------------------------------------------------------                
+    # ratio_pole
+    # convert array into dataframe 
+    DF4 = pd.DataFrame(ratio_poleAvg) 
+    # save the dataframe as a csv file 
+    DF4.to_csv(strBase + "ratioPole.csv")    
+    #---------------------------------------------------------                
